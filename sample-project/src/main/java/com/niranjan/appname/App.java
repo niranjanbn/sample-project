74814a4019f9aacc04bf8edd1b9e4aa2b465eb73
Niranjan <niranjanbn@gmail.com>
2018-06-05 11:27:12 -0500

    first commit

---
+++ sample-project/src/main/java/com/niranjan/appname/App.java
@@ -0,0 +1,13 @@
+package com.niranjan.appname;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
